3a2ee3e88d0af1ca296cbf33339050c0cfcea1b1
andrew leu <andrewwforsale@gmail.com>
2021-07-22 12:12:00 -0700

    add products page

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,14 @@ import Link from "next/link";
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
       <Link href="/">
-        <a>Home</a>
+        <a>Home </a>
+      </Link>
+      <Link href="/about">
+        <a>About </a>
+      </Link>
+      <Link href="/products">
+        <a>Products</a>
       </Link>
       <Component {...pageProps} />
       <footer>Andrew Leu was here</footer>
---
+++ pages/products.js
@@ -0,0 +1,15 @@
+const Products = () => {
+  return (
+    <>
+      <h1>Cool Product 1</h1>
+      <p>this is a cool Product</p>
+      <p>2,50</p>
+      <hr />
+      <h1>Cool Product 2</h1>
+      <p>this is an even cooler Product</p>
+      <p>250.00</p>
+    </>
+  );
+};
+
+export default Products;
